5272526ff0c8779cd8a6155466205789078eb95e
Megatruh <farhaneshaputra132@gmail.com>
2026-02-27 04:13:34 +0700

    Revert "feat: edit, delete, report; ui: resposifitas"
    This reverts commit 576afd8f7610f8b0b3297fee7be281fa88535d71.

--- logout.php
+++ logout.php
@@ -1,12 +1,18 @@
-<?php 
+<?php
 session_start();
+
+// Hapus semua session
 $_SESSION = [];
-session_unset();
 session_destroy();
 
-setcookie('id', '', time() - 3600);
-setcookie('key', '', time() - 3600);
+// Hapus cookie jika ada
+if (isset($_COOKIE['id'])) {
+    setcookie('id', '', time() - 3600, '/');
+}
+if (isset($_COOKIE['key'])) {
+    setcookie('key', '', time() - 3600, '/');
+}
 
+// Redirect ke login
 header("Location: login.php");
 exit;
-?>
